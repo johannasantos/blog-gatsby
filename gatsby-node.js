cd4ef07dfd8c1f7682abc48734348b982fe119e3
Johanna Santos <johannasabsantos@gmail.com>
2020-01-19 18:51:53 -0300

    gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,4 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
-
+const path = require(`path`)
 
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
@@ -20,4 +13,33 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
         })
     }
 }
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+    const result = await graphql(`
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `)
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+            path: node.fields.slug,
+            component: path.resolve(`./src/templates/blog-post.js`),
+            context: {
+                slug: node.fields.slug,
+            },
+        })
+    })
+}
+
+
+
 
